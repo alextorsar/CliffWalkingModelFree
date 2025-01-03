5758e6974699ddb198061e1c150774b89ab2ee53
alextorsar <alejandro.torres.sar@gmail.com>
2025-01-03 23:51:53 +0100

    Adding SARSA

--- SARSA/sarsa.py
+++ SARSA/sarsa.py
@@ -6,6 +6,12 @@ import random
 import numpy as np
 
 env = cliff_walking_wrapper()
+q = [[0 for _ in range(env.action_space.n)] for _ in range(env.state_space.n)]
+
+num_episodes = 500
+alpha = 0.1
+epsilon = 0.1
+gamma = 0.9
 
 def print_policy(policy):
     grid = [[' ' for _ in range(12)] for _ in range(4)]
@@ -28,28 +34,23 @@ def epison_greedy_policy(q, state, epsilon):
         max_index = np.argwhere(q[state] == np.max(q[state])).flatten()
         return random.choice(max_index)
 
-q = [[0 for _ in range(env.action_space.n)] for _ in range(env.state_space.n)]
-
-num_episodes = 500
-alpha = 0.1
-epsilon = 0.1
-gamma = 0.9
-
 for episode in range(num_episodes):
     state = env.get_random_initial_state()
+    action = epison_greedy_policy(q, state, epsilon)
     done = False
     while not done:
-        action = epison_greedy_policy(q, state, epsilon)
         next_state, reward, done = env.execute_action_from_state(state, action)
-        q[state][action] = (1-alpha)*q[state][action] + alpha*(reward + gamma*max(q[next_state]))
+        next_action = epison_greedy_policy(q, next_state, epsilon)
+        q[state][action] = (1-alpha)*q[state][action] + alpha*(reward + gamma*q[next_state][next_action])
         state = next_state
+        action = next_action
 
 policy = {}
 
 for state in range(env.state_space.n):
     policy[state] = np.argmax(q[state])
-
+    
 print_policy(policy)
 
 test_episodes = 5
-env.execute_episodes_with_policy(num_episodes = test_episodes, policy=policy)
+env.execute_episodes_with_policy(num_episodes = test_episodes, policy=policy)      
